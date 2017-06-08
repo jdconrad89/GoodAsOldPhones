f923db16c10e14e69c12eb6d64701d88f26841af
Jason Conrad <jason.d.conrad.89@gmail.com>
2017-06-08 13:56:19 -0600

    Initial Commit

---
+++ GoodAsOldPhones/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GoodAsOldPhones
+//
+//  Created by Jason Conrad on 6/8/17.
+//  Copyright © 2017 Jason Conrad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
