b4f803d763870f3aa69e9dfa858f485d56ecfc37
Jason Conrad <jason.d.conrad.89@gmail.com>
2017-06-14 13:08:37 -0600

    setup scroll functionality for the contact us page

--- GoodAsOldPhones/ContactViewController.swift
+++ GoodAsOldPhones/ContactViewController.swift
@@ -10,10 +10,18 @@ import UIKit
 
 class ContactViewController: UIViewController {
 
+    @IBOutlet weak var scrollView: UIScrollView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        view.addSubview(scrollView)
 
-        // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        
+        scrollView.contentSize = CGSize(width: 375, height: 800)
     }
 
 }
